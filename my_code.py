dcc81b1685a568079c70be5d88eb353901dd4d0c
Ottokore <Ottokore@mail.ru>
2025-04-07 13:40:28 +0300

    Sort added.

--- my_code.py
+++ my_code.py
@@ -6,22 +6,27 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def instance_initialization(input_file, filter):
+def instance_initialization(input_file, effective_filter):
     tree = ET.parse(input_file)
     root = tree.getroot()
 
-    return [
-        inst
-        for inst in root.findall(".//Instance")
-        if inst.find("Silicon") is None
-        or any(filter in s.text for s in inst.findall("Silicon"))
-        if (
-            inst.attrib.get("type") == "VirtualInstance"
-            or inst.attrib.get("xsi:type") == "VirtualInstance"
-            or inst.attrib.get("type") == "ComponentInstance"
-            or inst.attrib.get("xsi:type") == "ComponentInstance"
+    instances = [
+        inst for inst in root.findall(".//Instance")
+        if (inst.find("Silicon") is None or any(effective_filter in s.text for s in inst.findall("Silicon"))) and (
+            inst.attrib.get("type") == "VirtualInstance" or 
+            inst.attrib.get("xsi:type") == "VirtualInstance" or 
+            inst.attrib.get("type") == "ComponentInstance" or 
+            inst.attrib.get("xsi:type") == "ComponentInstance"
         )
     ]
+
+    sorted_instances = sorted(instances, key=lambda inst: (
+        (inst.attrib.get("type"), inst.attrib.get("xsi:type")),
+        (inst.find('ConceptName').text if inst.find('ConceptName') is not None else ''),
+        (inst.find('DesignName').text if inst.find('DesignName') is not None else '')
+    ))
+
+    return sorted_instances
 
 
 def main():
@@ -32,15 +37,13 @@ def main():
         input_file = "./instance_sheet_TC49x.xml"
         filter = find_filter(input_file='C:/python_projects/work/my_parcing/instance_sheet_TC49x.xml',filter = filter)
         data = open_lookup_file(Iproot)
-        
+        output_file = './output.xml'
         
         instances = instance_initialization(input_file, filter)
         
-        output_root = ET.Element("Instances")
+        output_root = ET.Element('Instances')
         for inst in instances:
             output_root.append(inst)
-
-        output_file = './output.xml'
 
         tree = ET.ElementTree(output_root)
         tree.write(output_file, encoding='utf-8', xml_declaration=True)
